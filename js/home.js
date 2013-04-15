a6fa028de42bf3ee6dbf09d24a256b6869c9a0a6
montchr <mont.chr@gmail.com>
2013-04-15 06:10:22 -0400

    sweet flexslider nav

--- js/home.js
+++ js/home.js
@@ -1,11 +1,12 @@
 jQuery(window).load(function() {
 	jQuery('.flexslider').flexslider({
-		animation : "slide",
-		easing : "linear",
-		direction : "vertical",
-		slideshowSpeed : 10000,
-		pauseOnHover : true,
-		controlNav : true,
-		directionNav : false
+		animation           : "slide",
+		easing              : "linear",
+		direction           : "vertical",
+		slideshowSpeed      : 10000,
+		pauseOnHover        : true,
+		controlNav          : true,
+		directionNav        : false
+		//controlsContainer   : ".flexslider-control-nav"
 	});
 });
