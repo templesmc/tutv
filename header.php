7f77add0a247eb2570ab26e425f15d09b9f0f171
Chris Montgomery <mont.chr@gmail.com>
2013-04-29 03:40:47 -0400

    header fixes. addresses many issues in #28. change doctype to HTML5. add modernize custom build. move favicon.ico to root dir
    removing as much dependency on thematic as seems wise
    
    ie8 is still fucked

--- header.php
+++ header.php
@@ -1,99 +1,86 @@
+<!doctype html>
+
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<!-- Google Chrome Frame for IE -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title><?php wp_title( '|', true, 'right' ); ?>Temple University Television</title>
+
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+	<!-- icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) -->
+	<!-- <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png"> -->
+	<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico">
+	<!--[if IE]>
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+	<![endif]-->
 <?php
 
-    // Creating the doctype
-    thematic_create_doctype();
-    echo " ";
-    language_attributes();
-    echo ">\n";
-    
-    // Creating the head profile
-    thematic_head_profile();
+// Creating the doc title
+//thematic_doctitle();
+//tutv_doctitle();
 
-    // Creating the doc title
-    //thematic_doctitle();
-    tutv_doctitle();
-    
-    // Creating the content type
-    thematic_create_contenttype();
-    
-    // Creating the description
-    thematic_show_description();
-    
-    // Creating the robots tags
-    thematic_show_robots();
-    
-    // Creating the canonical URL
-    thematic_canonical_url();
-    
-    // Loading the stylesheet
-    thematic_create_stylesheet();
-    
-    // Creating the internal RSS links
-    thematic_show_rss();
-    
-    // Creating the comments RSS links
-    thematic_show_commentsrss();
-    
-    // Creating the pingback adress
-    thematic_show_pingback();
-    
-    // Enables comment threading
-    thematic_show_commentreply();
+// Creating the description
+thematic_show_description();
 
-    // Calling WordPress' header action hook
-    wp_head();
-    
+// Loading the stylesheet
+//thematic_create_stylesheet();
+
+// Creating the internal RSS links
+thematic_show_rss();
+
+// Calling WordPress' header action hook
+wp_head();
+
 ?>
+
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/modernizr.custom.js"></script>
 
 </head>
 
 <?php 
 
 if (apply_filters('thematic_show_bodyclass',TRUE)) { 
-    // Creating the body class
-    ?>
+	// Creating the body class
+	?>
 
 <body class="<?php thematic_body_class() ?>">
-    
-<?php }
-
-// action hook for placing content before opening #wrapper
-thematic_before(); ?>
+	
+<?php } ?>
 
 <div id="wrapper" class="hfeed clearfix">
 
-    <?php
-    
-    // action hook for placing content above the theme header
-    thematic_aboveheader(); 
-    
-    ?>   
+	<div id="header" class="clearfix">
 
-    <div id="header" class="clearfix">
+		<div id="header-inner" class="container_12 clearfix">
+	
+			<?php 
+			
+			// action hook creating the theme header
+			thematic_header();
+			
+			?>
 
-        <div id="header-inner" class="container_12 clearfix">
-    
-            <?php 
-            
-            // action hook creating the theme header
-            thematic_header();
-            
-            ?>
+		</div> <!-- end #header-inner -->
+		
+	</div><!-- #header-->
+	
+	<?php
+	
+	// action hook for placing content below the theme header
+	thematic_belowheader();
+	
+	?>   
 
-        </div> <!-- end #header-inner -->
-        
-    </div><!-- #header-->
-    
-    <?php
-    
-    // action hook for placing content below the theme header
-    thematic_belowheader();
-    
-    ?>   
+	<div id="main" class="clearfix">
 
-    <div id="main" class="clearfix">
+		<?php thematic_abovecontainer(); ?>
 
-        <?php thematic_abovecontainer(); ?>
-
-        <div id="container" class="container_12 clearfix">
-    
+		<div id="container" class="container_12 clearfix">
+	
