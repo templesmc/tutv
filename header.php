7afaf5e7f68e763dc4918d265b8f7d9eb1cfb8aa
Chris Montgomery <mont.chr@gmail.com>
2013-05-14 17:59:32 -0400

    sep plugins need wp_title('') to work

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<!-- Google Chrome Frame for IE -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title><?php bloginfo('name'); ?></title>
+	<title><?php wp_title(''); ?></title>
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
