3570508e4625f64cc30b100827893fab15d197db
Chris Montgomery <chris@montchr.io>
2014-10-24 10:37:17 -0400

    don't lint unused and broken _vimeo-slideshow.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,8 @@ module.exports = function(grunt) {
         'Gruntfile.js',
         'assets/js/*.js',
         '!assets/js/*.min.js',
-        '!assets/js/_show.js'
+        '!assets/js/_show.js',
+        '!assets/js/_vimeo-slideshow.js'
       ]
     },
     sass: {
